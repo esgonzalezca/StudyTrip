692c59974ccc8d8aeaee27f7ddba0d7833adfe42
hadiazo <hadiazo@unal.edu.co>
2019-11-24 19:22:23 -0500

    Inicio de desacoplamiento. Experimento

---
+++ se1back/app/src/main/java/co/edu/unal/se1back/presentation/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package co.edu.unal.se1back.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.edu.unal.se1back.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
