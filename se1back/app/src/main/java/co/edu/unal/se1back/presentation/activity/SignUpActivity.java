68d6da6a1af0e4883c0ae39d1f516811ed0265ac
hadiazo <hadiazo@unal.edu.co>
2019-11-25 16:47:03 -0500

    Vista de sign up agregada

---
+++ se1back/app/src/main/java/co/edu/unal/se1back/presentation/activity/SignUpActivity.java
@@ -0,0 +1,14 @@
+package co.edu.unal.se1back.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import co.edu.unal.se1back.R;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
