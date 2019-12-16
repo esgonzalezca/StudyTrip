d3039348b6bba1bb3f7aef9a882c81119b027ebe
hadiazo <hadiazo@unal.edu.co>
2019-12-16 10:48:32 -0500

    Para el tag 2.0

---
+++ vistasBuscador_b1/Se1_project_D-Hubert_Feature/Tabs_Hubert/app/src/main/java/co/edu/unal/tabs_hubert/ui/main/PHF.java
@@ -0,0 +1,64 @@
+package co.edu.unal.tabs_hubert.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import co.edu.unal.tabs_hubert.R;
+
+import co.edu.unal.tabs_hubert.fragments.buscar_evento;
+import co.edu.unal.tabs_hubert.fragments.buscar_grupo;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PHF extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+
+    public static Fragment newInstance(int index) {
+        Fragment fragment = null;
+
+        switch (index) {
+            case 1:fragment=new buscar_grupo();break;
+            case 2:fragment=new buscar_evento();break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        final TextView textView = root.findViewById(R.id.section_label);
+        pageViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
