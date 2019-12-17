b88ecc8f453997fe20806e256d80552fe6130c0e
Sergio <esgonzalezca@unal.edu.co>
2019-12-17 15:21:49 +0200

    Preparing Final Release

--- gde-back/src/main/java/co/edu/unal/gdeback/repository/User_InterestRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unal.gdeback.repository;
-
-import co.edu.unal.gdeback.model.User_Interest;
-import co.edu.unal.keys.User_Interest_Id;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface User_InterestRepository extends JpaRepository<User_Interest, User_Interest_Id> {}
